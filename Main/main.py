3a9ed96a82df5c1b4c83a8a360d73c2f72e7976c
AmirHosein <awir.software@gmail.com>
2022-12-25 12:47:33 +0330

    add media group with caption

--- Main/main.py
+++ Main/main.py
@@ -3,7 +3,7 @@ from decouple import config
 # config api token bot
 api_token = config("API_TOKEN")
 
-from telegram import Update
+from telegram import Update, InputMediaPhoto
 from telegram.ext import CallbackContext, Updater, dispatcher, CommandHandler
 
 # requests library
@@ -12,6 +12,12 @@ import requests
 # set telegram bot
 updater = Updater(api_token)
 dispatcher = updater.dispatcher
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton
+inline_keys = [
+    [InlineKeyboardButton('Search Movie', callback_data='1'), InlineKeyboardButton('About IMDB Bot', callback_data='2')],
+    [InlineKeyboardButton('Cancel', callback_data='3')]
+]
 
 # start function
 def start(update:Update, context:CallbackContext):
@@ -24,13 +30,23 @@ to get information about your favorite movie
 please enter the name of the movie:
 :)
     '''
-    context.bot.send_message(chat_id, text)
+    # Add buttons
+    inline_markup = InlineKeyboardMarkup(inline_keys)
+    update.message.reply_text(text=text, reply_markup=inline_markup)
 
-from package import getMovie, getStatus
+from package import getMovie, getStatus,getpic,getPicture
 def movie(update:Update, context:CallbackContext):
     chat_id = update.message.chat_id
     result = getMovie('Troll')
-    context.bot.send_message(chat_id, result[0])
+    media_group = []
+    if result[1] != "-1":
+        text = result[0]
+        url = f'https://www.themoviedb.org/t/p/w600_and_h900_bestv2{result[1]}'
+        getPicture(url=url)
+        media_group.append(InputMediaPhoto(open('new.jpg', 'rb'),caption = text))
+        context.bot.send_media_group(chat_id = chat_id, media = media_group)
+    else:
+        context.bot.send_message(chat_id, result[0]);
 
 # set command for start
 dispatcher.add_handler(CommandHandler('start', start))
--- Main/package.py
+++ Main/package.py
@@ -1,9 +1,27 @@
 # requests library
 import requests
 
+#import packge for download image from url
+from PIL import Image
+import requests
+import urllib.request
+from io import BytesIO
+
+def getpic(url):
+    response = requests.get(url)
+    img = Image.open(response.content,'rb')
+    return img
+    
+def getPicture(url):
+    urllib.request.urlretrieve(url,"new.jpg")
+    img = Image.open("new.jpg")
+
+
+
 # get movie from TMDB Api website
 def getMovie(name):
     api_key = '2daf17455648c676bffaf192d612608c'
+    
     try:
         response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={name}&language=en-US&page=1&include_adult=false')
     except:
